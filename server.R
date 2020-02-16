0939e37b8cb4bdd936881f6694fe85188b0f0c6c
bbhusal <47510217+bbhusal@users.noreply.github.com>
2020-02-16 15:30:08 -0500

    Create server.R

---
+++ server.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(plotly)
+library(ggplot2)
+
+mpgData <- mtcars
+mpgData$cyl <- factor(mpgData$cyl,labels = c("Four","Six","Eight"))
+mpgData$am<- factor(mpgData$am,labels = c("Automatic","Manual"))
+mpgData$vs <- factor(mpgData$vs, labels = c("V-shaped","Straight"))
+mpgData$gear<- factor(mpgData$gear,labels = c("3","4","5"))
+mpgData$carb<-factor(mpgData$carb,labels =c("1","2","3","4","6","8"))
+
+shinyServer(function(input, output) {
+    formulaText <- reactive({
+        paste("mpg vs ", input$variable)
+    })
+    
+    output$caption <- renderText({
+        formulaText()
+    })
+    
+    
+    output$mpgPlot <- renderPlot({
+        ggplot(mpgData, aes_string(x=input$variable, y="mpg", fill= input$variable)) + geom_boxplot()+
+            labs(title="Boxplots of Miles Per Gallon",y="Miles Per Gallon")+
+                     theme_classic()
+                 
+    })
+        output$data<- renderTable({
+            mpgData[,c("mpg",input$variable)]
+        })
+})
